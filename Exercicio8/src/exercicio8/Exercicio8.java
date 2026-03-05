0b229dbaef74622e4e79c26732064bdceac998b6
maxxvinycius <maxvinycius36@gmail.com>
2026-03-05 16:29:58 -0300

    Add files via upload

---
+++ Exercicio8/src/exercicio8/Exercicio8.java
@@ -0,0 +1,17 @@
+
+package exercicio8;
+
+
+public class Exercicio8 {
+
+    
+    public static void main(String[] args) {
+        Porta p = new Porta();
+        
+        p.abrir();
+        System.out.println("a porta está aberta!");
+        p.fechar();
+        System.out.println("a porta está fechada.");
+        }
+    
+}
---
+++ Exercicio8/src/exercicio8/Porta.java
@@ -0,0 +1,16 @@
+
+package exercicio8;
+
+
+public class Porta {
+    private boolean aberta;
+    
+    
+    public void abrir(){
+        aberta = true;
+    }
+    public void fechar(){
+        aberta = false;
+    
+ }
+}
