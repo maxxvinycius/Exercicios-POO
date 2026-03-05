751b0ec6c4ffbcfefc4213e71ca3a7032b8535ed
maxxvinycius <maxvinycius36@gmail.com>
2026-03-05 16:44:34 -0300

    Add files via upload

---
+++ Exercicio9/src/exercicio9/Exercicio9.java
@@ -0,0 +1,16 @@
+
+package exercicio9;
+
+
+public class Exercicio9 {
+
+    
+    public static void main(String[] args) {
+        Cachorro c = new Cachorro();
+        Gato g = new Gato();
+        
+        c.fazerSom();
+        g.fazerSom();
+    }
+    
+}
